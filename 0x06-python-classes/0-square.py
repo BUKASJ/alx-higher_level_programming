18b3a455fe60f6dfe23915646222307c8aa82360
BUKASJ <chukwuebukaedej@gmail.com>
2024-02-06 15:08:43 -0800

    empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-"""
-This module defines a Square class
-"""
+"""This module defines a Square class"""
 
 class Square:
-    """
-    This class represents a square
-    """
+    """This class represents a square"""
     pass
