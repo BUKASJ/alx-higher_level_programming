d576783bbc666ea1c6f3f9357019c62fb9381715
Bukasj <chukwuebukaedej@gmail.com>
2024-03-06 07:30:44 -0800

    defines a rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 class Rectangle:
+
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
-        return (self.__width)
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -20,7 +21,7 @@ class Rectangle:
 
     @property
     def height(self):
-        return (self.__height)
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -28,13 +29,14 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     def area(self):
-        return (self.width * self.height)
+        return self.height * self.width
 
     def perimeter(self):
-        if (self.width != 0 and self.height != 0):
-            return (2 * (self.width + self.height))
-        else:
-            return (0)
+        if self.height == 0 or self.width == 0:
+            return 0
+
+        return 2 * (self.height + self.width)
