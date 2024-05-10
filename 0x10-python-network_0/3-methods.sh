f086c1bd8990cf0b92af3cd8f7b6b10f354f675b
BUKASJ <chukwuebukaedej@gmail.com>
2024-05-09 19:42:27 -0700

    takes in a URL and displays all HTTP methods the server will accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept
-curl -s -I --options "$1" | grep -i 'Allow:' | awk '{print $2}'
+curl -s -I "$1" | grep -i Allow | cut -d ' ' -f2-
