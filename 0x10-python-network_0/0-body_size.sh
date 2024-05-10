7f0f4c543ab7eb41b580b0bec84b5d5441735003
BUKASJ <chukwuebukaedej@gmail.com>
2024-05-09 17:44:48 -0700

    takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,8 @@
 #script that takes in a URL, sends a request to that URL,
 #and displays the size of the body of the response
 
-curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
+url=$1
+
+response=$(curl -sI $url | grep -i 'content-length' | awk '{print $2}' | tr -d '\r')
+
+echo "Size of the response body: $response bytes"
