d8bdb7ea9a4e3780963cf76c322f05e20abd9676
bukasj <chukwuebukaedej@gmail.com>
2024-05-11 12:02:04 +0100

    takes in a URL, sends a GET request to the URL

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -X GET "$1" -w "%{http_code}" | awk '/200/ {p=1; next} p' | sed '1d'
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response if the status code is 200
+curl -sL "$1" -X GET -D - -o /dev/null | grep "200 OK" && curl -sL "$1"
