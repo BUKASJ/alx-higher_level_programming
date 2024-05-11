ad3845d1673340df76494a7cd7455bff134bfbb3
BUKASJ <chukwuebukaedej@gmail.com>
2024-05-10 19:18:16 -0700

    script that fetches url and displays the response body

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Fetches and displays the response body from a URL."""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        data = response.read().decode('utf-8')
+
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
