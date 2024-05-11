58ce72a6f8f0361e1824b5982eaf19f09143c884
BUKASJ <chukwuebukaedej@gmail.com>
2024-05-10 21:12:50 -0700

    script that takes 2 arguments

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script takes two arguments: the repository name and the owner name,
+and uses the GitHub API to list 10 commits (from the most recent to oldest)
+of the specified repository by the given owner.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()[:10]
+        for commit in commits:
+            sha = commit.get('sha')
+            author_name = commit.get('commit').get('author').get('name')
+            print(f"{sha}: {author_name}")
+    else:
+        print("Error:", response.status_code)
